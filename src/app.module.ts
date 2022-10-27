8087882ccb4a89d3d6156a5586eac3131de3a4a7
Thiago Santos <thiago.dsantos@outlook.com>
2022-10-27 19:33:30 -0300

    Remove examples files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from 'src/app.controller';
-import { AppService } from 'src/app.service';
 import { EventsModule } from "src/events/events.module";
 import { RoomModule } from "src/modules/room/room.module";
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [EventsModule, RoomModule, ConfigModule.forRoot({ envFilePath: process.env.ENV_FILE })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EventsModule,
+    RoomModule, ConfigModule.forRoot({ envFilePath: process.env.ENV_FILE })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
