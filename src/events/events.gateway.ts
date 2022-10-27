8b33113412bebd326f05d89e708b922d20a94ece
Thiago Santos <thiago.dsantos@outlook.com>
2022-10-27 19:24:42 +0000

    Using environment variables

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -3,26 +3,21 @@ import { BadRequestException } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { RoomService } from "src/modules/room/room.service";
 
-@WebSocketGateway(8003, { cors: { origin: '*' } })
+const port = (process.env.SOCKET_PORT ?? 8003) as number;
+
+@WebSocketGateway(port, { cors: { origin: '*' } })
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
-  
-  constructor(private readonly roomService: RoomService) {}
-  
+
+  constructor(private readonly roomService: RoomService) { }
+
   @SubscribeMessage('room')
   async room(@MessageBody() message: any) {
     if (!message.event) {
       throw new BadRequestException('Invalid event');
     }
-    
+
     return await this.roomService.execute(message.event, message.data);
-    
-    /**
-     * TODO: criar os seguintes métodos para sala
-     * - create para criação da sala
-     * - entrar na sala
-     * - encerrar sala
-     */
   }
 }
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -10,12 +10,12 @@ import { RoomRepository } from "src/modules/room/room.repository";
     CacheModule.register<ClientOpts>({
       store: async () => await redisStore({
         socket: {
-          host: 'localhost',
-          port: 6379,
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
         }
       }),
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     })
   ],
   providers: [
@@ -24,4 +24,4 @@ import { RoomRepository } from "src/modules/room/room.repository";
     RoomRepository
   ]
 })
-export class EventsModule {}
+export class EventsModule { }
