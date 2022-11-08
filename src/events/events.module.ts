9a9df78c8800097ae05c8bafba28540661b50166
Thiago Santos <thiago.dsantos@outlook.com>
2022-11-08 01:49:51 -0300

    Fix Redis host and port

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -7,18 +7,19 @@ import { RoomRepository } from 'modules/room/room.repository';
 import { MatchService } from 'modules/match/match.service';
 import { MatchRepository } from 'modules/match/match.repository';
 
-const REDIS_PORT = (process.env.REDIS_HOST ?? 6379) as number;
+const REDIS_HOST = (process.env.REDIS_HOST ?? 'localhost') as string;
+const REDIS_PORT = (process.env.REDIS_PORT ?? 6379) as number;
 
 @Module({
   imports: [
     CacheModule.register<ClientOpts>({
       store: async () => await redisStore({
         socket: {
-          host: process.env.REDIS_HOST,
+          host: REDIS_HOST,
           port: REDIS_PORT,
         }
       }),
-      host: process.env.REDIS_HOST,
+      host: REDIS_HOST,
       port: REDIS_PORT,
     }),
   ],
